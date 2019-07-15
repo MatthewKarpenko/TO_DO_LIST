fb4082fa17c9d8588649b31a1ded0766ed714a34
MatthewKarpenko <mtw.karp@gmail.com>
2019-07-15 15:18:40 +0300

    Updates

---
+++ precache-manifest.9115e05378139d98d9669b8371f096be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0fc711891ea3fee3e4d891b502853e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ab342de34ce3c35d08a",
+    "url": "/static/css/main.85cdb3fd.chunk.css"
+  },
+  {
+    "revision": "1de69534242d76622ebf",
+    "url": "/static/js/2.dadebc93.chunk.js"
+  },
+  {
+    "revision": "2ab342de34ce3c35d08a",
+    "url": "/static/js/main.ec3f4be6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/apis/axiosConfig.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'http://159.89.96.181',
-    headers: {"Authorization": "Bearer tk25nfhzce216itz16njk1rwyueoroipirot"}
-})
